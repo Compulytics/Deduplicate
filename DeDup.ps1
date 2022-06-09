f3f1fbb1807779cb455e840f5adcc81628fbca66
Compulytics <98241476+Compulytics@users.noreply.github.com>
2022-06-09 11:53:44 -0400

    Update to include file sizes
    Will now print a line with the total size of duplicate files as well as includes file sizes (in bytes) in the report.

--- DeDup.ps1
+++ DeDup.ps1
@@ -2,14 +2,16 @@ function Deduplicate{
 	param([string]$FilePath, [string]$Action, [string]$NewDir)
 	$FileTable = @()
 	$HashTable = @{}
+	$TotalDuplicateSize = 0
 	Get-ChildItem $FilePath -Recurse -Force | Sort-Object -Property FullName -Descending | ForEach-Object {
 		try {
 			$CurrentFile = $_.FullName
+			$CurrentLength = $_.Length
 			$CurrentHash = Get-FileHash $_.FullName -Algorithm MD5 | Select Hash
 			$CurrentHashValue = $CurrentHash.Hash
 			if ($CurrentHash.Hash){
 				if ($CurrentHashValue -ne "D41D8CD98F00B204E9800998ECF8427E"){
-					$FilePair = "$CurrentHashValue|$CurrentFile"
+					$FilePair = "$CurrentHashValue|$CurrentFile|$CurrentLength"
 					$FileTable += "$FilePair"
 					Write-Host "Processed File $_"
 				}
@@ -22,21 +24,23 @@ function Deduplicate{
 	foreach ($HashName in $FileTable){
 		$I=1
 		$Hash = $HashName.Split("|")[-0]
-		$File = $HashName.Split("|")[-1]
+		$File = $HashName.Split("|")[-2]
+		$Size = $HashName.Split("|")[-1]
 		if ($HashTable.ContainsKey($Hash)){
 			$WriteHash = $HashTable[$Hash]
+			$TotalDuplicateSize += $Size
 			if ($Action -eq "D"){
 				Remove-Item -Path $File -Force -ErrorAction Stop
 				Write-Host "$File Deleted."
-				Add-Content Dedup_Report.csv "$WriteHash,$File,$Hash,Deleted"
+				Add-Content Dedup_Report.csv "$WriteHash,$File,$Size,$Hash,Deleted"
 			}
 			elseif ($Action -eq "L"){
 				Write-Host "$File Duplicate Logged."
-				Add-Content Dedup_Report.csv "$WriteHash,$File,$Hash,Logged"	
+				Add-Content Dedup_Report.csv "$WriteHash,$File,$Size,$Hash,Logged"	
 			}
 			elseif ($Action -eq "C"){
 				Write-Host "$File Duplicate Not Copied."
-				Add-Content Dedup_Report.csv "$WriteHash,$File,$Hash,Not Copied"
+				Add-Content Dedup_Report.csv "$WriteHash,$File,$Size,$Hash,Not Copied"
 			}
 			else{
 				Write-Host "ERROR: Invalid Action."
@@ -64,6 +68,35 @@ function Deduplicate{
 			}
 		}
 	}
+	if ($TotalDuplicateSize -lt 1024){
+		Write-Host "*******************************************************"
+		Write-Host "Total duplicate size: $TotalDuplicateSize Bytes"
+		Write-Host "*******************************************************"
+	}
+	elseif ($TotalDuplicateSize -lt 1048576){
+		$TotalDuplicateSize = [math]::Round($TotalDuplicateSize/1024)
+		Write-Host "*******************************************************"
+		Write-Host "Total duplicate size: $TotalDuplicateSize KiloBytes"
+		Write-Host "*******************************************************"
+	}
+	elseif ($TotalDuplicateSize -lt 1073741824){
+		$TotalDuplicateSize = [math]::Round($TotalDuplicateSize/1048576)
+		Write-Host "*******************************************************"
+		Write-Host " Total duplicate size: $TotalDuplicateSize MegaBytes"
+		Write-Host "*******************************************************"
+	}
+	elseif ($TotalDuplicateSize -lt 137438953472){
+		$TotalDuplicateSize = [math]::Round($TotalDuplicateSize/1073741824)
+		Write-Host "*******************************************************"
+		Write-Host " Total duplicate size: $TotalDuplicateSize GigaBytes"
+		Write-Host "*******************************************************"
+	}
+	else{
+		$TotalDuplicateSize = [math]::Round($TotalDuplicateSize/137438953472)
+		Write-Host "*************************************************"
+		Write-Host " Total duplicate size: $TotalDuplicateSize TeraBytes"
+		Write-Host "*************************************************"
+	}
 }
 function HELP{
 	Write-Host "USAGE:"
@@ -86,17 +119,17 @@ if ($UserInput_FilePath){
 	if ($UserInput_Action){
 		if (Test-Path -Path $UserInput_FilePath) {
 			if ($UserInput_Action -eq "D"){
-				Add-Content Dedup_Report.csv 'Original File,Duplicate File,File Hash,Action'
+				Add-Content Dedup_Report.csv 'Original File,Duplicate File,Size,File Hash,Action'
 				Deduplicate $UserInput_FilePath $UserInput_Action
 			}
 			elseif ($UserInput_Action -eq "L"){
-				Add-Content Dedup_Report.csv 'Original File,Duplicate File,File Hash,Action'
+				Add-Content Dedup_Report.csv 'Original File,Duplicate File,Size,File Hash,Action'
 				Deduplicate $UserInput_FilePath $UserInput_Action
 			}
 			elseif ($UserInput_Action -eq "C"){
 				if ($UserInput_NewDir){
 					if (Test-Path -Path $UserInput_NewDir) {
-							Add-Content Dedup_Report.csv 'Original File,Duplicate File,File Hash,Action'
+							Add-Content Dedup_Report.csv 'Original File,Duplicate File,Size,File Hash,Action'
 							Deduplicate $UserInput_FilePath $UserInput_Action $UserInput_NewDir
 					}
 					else{
